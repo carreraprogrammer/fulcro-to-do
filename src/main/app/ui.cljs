33dd18b41a878745c6ddcff97979ad4fb710d056
carreraprogrammer <carreraprogrammer@gmail.com>
2023-02-13 19:30:38 -0500

    Hardcode initial state

--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -3,5 +3,9 @@
   (:require
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
     [com.fulcrologic.fulcro.dom :as dom]))
-(defsc Root [this props]
-       (dom/div "TODO"))
+(defsc Root [this state]
+  (let [todo-data {:todos {:list/label "To-do List" :list/todos
+                           [{:todo/id 1 :todo/task "Do the dishes" :todo/done false}
+                            {:todo/id 2 :todo/task "Buy groceries" :todo/done false}
+                            {:todo/id 3 :todo/task "Take out the trash" :todo/done true}]}}]
+    (dom/div "IT'S WORKING!")))
