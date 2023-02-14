f2b6b94ba03b0b04bfbb6f9665f9bcd2931f09b1
carreraprogrammer <carreraprogrammer@gmail.com>
2023-02-13 21:35:49 -0500

    Render todo list using ui-list factory function

--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -1,30 +1,29 @@
-
 (ns app.ui
   (:require
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
     [com.fulcrologic.fulcro.dom :as dom]))
 
 (defsc Todo [this {:todo/keys [id task done]}]
-  (dom/label (
-               (dom/input {:type    "checkbox"
-                           :checked done
-                           :onClick (fn [] println "DONE: " task)
-                           })
-               (dom/span task)
-               )))
+  (dom/li
+   (dom/label
+    (dom/input {:type    "checkbox"
+                :checked done
+                :onClick (fn [] (println "DONE: " task))
+                })
+    (dom/span task))))
 
-(def ui-todo (comp/factory Todo {:keyfn :todo/id}))         ;With this function I can create instances of the Todo component
+(def ui-todo (comp/factory Todo {:keyfn :todo/id}))
 
-(defsc Todo-list [this {:list/keys [label todos]}]
+(defsc TodoList [this {:list/keys [label todos]}]
   (dom/div
-    ((dom/h1 label)
-     (dom/ul (map ui-todo todos)))))                        ; Create instances from the list of todos
+    (dom/h1 label)
+    (dom/ul (map ui-todo todos))))
 
-(def ui-list (comp/factory Todo-list))
+(def ui-list (comp/factory TodoList))
 
 (defsc Root [this state]
   (let [todo-data {:todos {:list/label "FULCRO TODO" :list/todos
                            [{:todo/id 1 :todo/task "Do the dishes" :todo/done false}
                             {:todo/id 2 :todo/task "Buy groceries" :todo/done false}
                             {:todo/id 3 :todo/task "Take out the trash" :todo/done true}]}}]
-    (dom/div "IT'S WORKING!")))
+    (dom/div (ui-list (:todos todo-data)))))
