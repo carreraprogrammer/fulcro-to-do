9264425ded95551c6cff49e592aba0c8db276ed6
carreraprogrammer <carreraprogrammer@gmail.com>
2023-02-13 19:02:20 -0500

    Chunk client in application and ui modules

---
+++ src/main/app/ui.cljs
@@ -0,0 +1,7 @@
+
+(ns app.ui
+  (:require
+    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
+    [com.fulcrologic.fulcro.dom :as dom]))
+(defsc Root [this props]
+       (dom/div "TODO"))
