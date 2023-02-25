36f1a9777a13967e743b5267cfb9d845b2c00df9
carreraprogrammer <carreraprogrammer@gmail.com>
2023-02-25 11:48:04 -0500

    Add query to list todos

--- Datomic/db/todos_db/schema.clj
+++ Datomic/db/todos_db/schema.clj
@@ -13,7 +13,7 @@
 (def todo-schema
   [{:db/ident       :list/id
     :db/valueType   :db.type/long
-    :db/cardinality :db.cardinality/one
+    :db/cardinality :db.cardinality/many
     :db/unique      :db.unique/identity}
 
    {:db/ident       :list/title
@@ -32,11 +32,7 @@
     :db/valueType   :db.type/boolean
     :db/cardinality :db.cardinality/one
     :db/doc         "Whether the todo is done or not"}
-
-   {:db/ident       :list/todos
-    :db/valueType   :db.type/ref
-    :db/cardinality :db.cardinality/many
-    :db/doc         "The lists that this todo belongs to"}])
+   ])
 
 (d/transact conn {:tx-data todo-schema})
 
@@ -44,26 +40,52 @@
 
 ;; add a new todo function
 
-(defn upsert-todo!
-  "Update or insert one record"
+(defn insert-todo!
+  "Insert one todo"
   [conn {:keys [todo-id text done list-id]}]
-  (d/transact conn {:tx-data [[:db/add "temporary-new-db-id" :todo/id todo-id]
-                              [:db/add "temporary-new-db-id" :todo/text text]
-                              [:db/add "temporary-new-db-id" :todo/done done]
-                              [:db/add "temporary-new-db-id" :list/id list-id]
+  (d/transact conn {:tx-data [[:db/add todo-id :todo/id todo-id]
+                              [:db/add todo-id :todo/text text]
+                              [:db/add todo-id :todo/done done]
+                              [:db/add list-id :list/id list-id]
                               ]}))
 
 ;; add my first todo
 
-(upsert-todo! conn {:todo-id 5
-                    :text "take a nap"
-                    :done false
-                    :list-id 0})
+(insert-todo! conn {:todo-id 0
+                    :text "make website"
+                    :done true
+                    :list-id 0
+                    })
 
-(d/q '[:find ?text ?done
-       :where [?todo :todo/text ?text]
-       [?todo :todo/done ?done]]
+(insert-todo! conn {:todo-id 1
+                    :text "do the laundry"
+                    :done true
+                    :list-id 0
+                    })
+
+(insert-todo! conn {:todo-id 2
+                    :text "Walk the dog"
+                    :done true
+                    :list-id 0
+                    })
+
+(insert-todo! conn {:todo-id 3
+                    :text "watch seriesss"
+                    :done true
+                    :list-id 0
+                    })
+
+(d/q '[:find ?e ?todo-id ?text ?done
+       :where [?todo-id :todo/text ?text]
+       [?todo-id :todo/id ?todo-id]
+       [?todo-id :todo/done ?done]
+       [?e :list/id _]]
      (d/db conn))
+
+
+; => [[0 2 "Walk the dog" true] [0 0 "make website" true] [0 1 "do the laundry" true] [0 3 "watch series" true]]
+
+
 
 
 
