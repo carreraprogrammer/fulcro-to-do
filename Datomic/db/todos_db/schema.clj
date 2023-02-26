69b32ddbb574cbfc416afc5692a186ccf389e778
carreraprogrammer <carreraprogrammer@gmail.com>
2023-02-25 21:50:18 -0500

    Modify database schema and fix upsert-todo!

--- Datomic/db/todos_db/schema.clj
+++ Datomic/db/todos_db/schema.clj
@@ -1,5 +1,6 @@
 (ns todos-db.schema
-  (:require [datomic.client.api :as d]))
+  (:require [datomic.client.api :as d])
+  (:import (java.util UUID Date)))
 
 (def client (d/client {:server-type :dev-local
                        :system "dev"
@@ -11,82 +12,125 @@
 (def conn (d/connect client {:db-name "todo-app"}))
 
 (def todo-schema
-  [{:db/ident       :list/id
-    :db/valueType   :db.type/long
-    :db/cardinality :db.cardinality/many
-    :db/unique      :db.unique/identity}
-
-   {:db/ident       :list/title
-    :db/valueType   :db.type/string
-    :db/cardinality :db.cardinality/one}
-
-   {:db/ident       :todo/id
-    :db/valueType   :db.type/long
-    :db/cardinality :db.cardinality/one}
-
-   {:db/ident       :todo/text
-    :db/valueType   :db.type/string
-    :db/cardinality :db.cardinality/one}
-
-   {:db/ident       :todo/done
-    :db/valueType   :db.type/boolean
-    :db/cardinality :db.cardinality/one
-    :db/doc         "Whether the todo is done or not"}
-   ])
+  [{:db/id ":list/id"
+    :db/ident :list/id
+    :db/valueType {:db/ident :db.type/long}
+    :db/cardinality {:db/ident :db.cardinality/one}
+    :db/unique {:db/ident :db.unique/identity}}
+   {:db/id ":list/title"
+    :db/ident :list/title
+    :db/valueType {:db/ident :db.type/string}
+    :db/cardinality {:db/ident :db.cardinality/one}}
+   {:db/id ":list/todos"
+    :db/ident :list/todos
+    :db/valueType {:db/ident :db.type/ref}
+    :db/cardinality {:db/ident :db.cardinality/many}}
+   {:db/id ":todo/id"
+    :db/ident :todo/id
+    :db/valueType {:db/ident :db.type/long}
+    :db/cardinality {:db/ident :db.cardinality/one}
+    :db/unique {:db/ident :db.unique/identity}}
+   {:db/id ":todo/text"
+    :db/ident :todo/text
+    :db/valueType {:db/ident :db.type/string}
+    :db/cardinality {:db/ident :db.cardinality/one}}
+   {:db/id ":todo/done"
+    :db/ident :todo/done
+    :db/valueType {:db/ident :db.type/boolean}
+    :db/cardinality {:db/ident :db.cardinality/one}}
+   {:db/id ":list/todos+id"
+    :db/ident :list/todos+id
+    :db/valueType {:db/ident :db.type/tuple}
+    :db/cardinality {:db/ident :db.cardinality/one}
+    :db/tupleAttrs [:list/id :todo/id]}
+   {:db/id ":list/id+title"
+    :db/ident :list/id+title
+    :db/valueType {:db/ident :db.type/tuple}
+    :db/cardinality {:db/ident :db.cardinality/one}
+    :db/unique {:db/ident :db.unique/identity}
+    :db/tupleAttrs [:list/id :list/title]}])
 
 (d/transact conn {:tx-data todo-schema})
 
-(d/list-databases client {})
+(defn create-list!
 
-;; add a new todo function
+  [conn {:keys [id title]}]
 
-(defn insert-todo!
+  (d/transact conn {
+                    :tx-data [
+                              [:db/add id :list/id id]
+                              [:db/add id :list/title title]
+                              ]
+                    })
+  )
+(create-list! conn {:id 0
+                   :title "TODOS"
+                    })
+
+(d/q '[:find ?id ?title       :where [?e :list/title ?title] [?e :list/id ?id]]
+     (d/db conn))
+
+;=> [[#uuid"9d75b380-358c-4440-9abe-e25e9a95529a" "TODOS"] [#uuid"6413c235-e963-4815-a766-d72836ed0470" "TODOS 2"]]
+
+(defn upsert-todo!
   "Insert one todo"
-  [conn {:keys [todo-id text done list-id]}]
-  (d/transact conn {:tx-data [[:db/add todo-id :todo/id todo-id]
-                              [:db/add todo-id :todo/text text]
-                              [:db/add todo-id :todo/done done]
-                              [:db/add list-id :list/id list-id]
+  [conn {:keys [todo-id text done list-id title]}]
+  (d/transact conn {:tx-data [[:db/add "temporary-new-db-id" :todo/id todo-id]
+                              [:db/add "temporary-new-db-id" :todo/text text]
+                              [:db/add "temporary-new-db-id" :todo/done done]
+                              [:db/add 0 :list/todos+id [list-id todo-id]]
+                              [:db/add 0 :list/id+title [list-id title]]
                               ]}))
 
 ;; add my first todo
 
-(insert-todo! conn {:todo-id 0
+(upsert-todo! conn {:todo-id 0
                     :text "make website"
                     :done true
                     :list-id 0
+                    :title "TODOS"
                     })
 
-(insert-todo! conn {:todo-id 1
+(upsert-todo! conn {:todo-id 1
                     :text "do the laundry"
                     :done true
                     :list-id 0
+                    :title "TODOS"
                     })
 
-(insert-todo! conn {:todo-id 2
+(upsert-todo! conn {:todo-id 2
                     :text "Walk the dog"
                     :done true
                     :list-id 0
+                    :title "TODOS"
                     })
 
-(insert-todo! conn {:todo-id 3
-                    :text "watch seriesss"
+(upsert-todo! conn {:todo-id 4
+                    :text "clean my bedroom"
                     :done true
                     :list-id 0
+                    :title "TODOS"
                     })
 
-(d/q '[:find ?e ?todo-id ?text ?done
-       :where [?todo-id :todo/text ?text]
-       [?todo-id :todo/id ?todo-id]
-       [?todo-id :todo/done ?done]
-       [?e :list/id _]]
+(upsert-todo! conn {:todo-id 5
+                    :text "watch series"
+                    :done true
+                    :list-id 0
+                    :title "TODOS"
+                    })
+
+(d/q '[:find ?list-title ?todo-id ?text ?done
+       :where
+       [?e :todo/text ?text]
+       [?e :todo/id ?todo-id]
+       [?e :todo/done ?done]
+       [_ :list/title ?list-title]
+     ]
      (d/db conn))
 
-
-; => [[0 2 "Walk the dog" true] [0 0 "make website" true] [0 1 "do the laundry" true] [0 3 "watch series" true]]
-
-
-
-
-
-
+;=>=>
+;[["TODOS" 4 "clean my bedroom" true]
+; ["TODOS" 0 "make website" true]
+; ["TODOS" 5 "watch series" true]
+; ["TODOS" 2 "Walk the dog" true]
+; ["TODOS" 1 "do the laundry" true]]
